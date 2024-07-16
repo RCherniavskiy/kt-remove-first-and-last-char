99aee72a73ff42a67773ebbb7a980efc7fd76db2
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-07-16 12:09:07 +0300

    add fun1

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,13 +1,5 @@
 package mate.academy
 
-fun main() {
-    println(removeChars("book"))   // "oo"
-    println(removeChars("house"))  // "ous"
-    println(removeChars("us"))     // ""
-    println(removeChars("a"))      // ""
-    println(removeChars(""))       // ""
-}
-
 fun removeChars(str: String): String {
     if (str.length <= 2) {
         return ""
